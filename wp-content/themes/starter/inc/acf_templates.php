b8821e3b3ec97ac85a9caffb73eeb59d6c74dff9
akram-tello <a.tello@graduate.utm.my>
2025-04-05 14:49:06 +0800

    first init starter theme

---
+++ wp-content/themes/starter/inc/acf_templates.php
@@ -0,0 +1,12 @@
+<?php
+
+function starter_theme_block_callback( $block ) {
+    
+    // convert name ("acf/testimonial") into path friendly slug ("testimonial")
+    $slug = str_replace('acf/', '', $block['name']);
+    
+    // include a template part from within the "template-parts/block" folder
+    if( file_exists( get_theme_file_path("/blocks/module-{$slug}.php") ) ) {
+        include( get_theme_file_path("/blocks/module-{$slug}.php") );
+    }
+}
